a24b605e45b4ab3ad87ba0617b93dda3803dc2b6
Erick Malta <erickmalta100@gmail.com>
2020-12-12 00:07:29 -0300

    fix(app): add collection name to uri

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { PlayersModule } from './players/players.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017', {
+    MongooseModule.forRoot('mongodb://localhost:27017/smartranking', {
       useNewUrlParser: true,
       useCreateIndex: true,
       useUnifiedTopology: true,
