70b13aaedc31b2d584d106cc1571a912ac89f77e
Erick Malta <erickmalta100@gmail.com>
2020-12-11 22:53:31 -0300

    feat(app): add mongo connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PlayersModule } from './players/players.module';
 
 @Module({
-  imports: [PlayersModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017', {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    }),
+    PlayersModule,
+  ],
   controllers: [],
   providers: [],
 })
