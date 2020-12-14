ca404534ec38643242f3c9942955909c245a4229
Erick Malta <erickmalta100@gmail.com>
2020-12-13 23:27:51 -0300

    feat(categories): add get methods

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,10 +1,13 @@
 import {
   Body,
   Controller,
+  Get,
+  Param,
   Post,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
+import { FindParamDTO } from '../shared/dtos/find-param.dto';
 import { CategoriesService } from './categories.service';
 import { CreateCategoryDTO } from './dtos/create-category.dto';
 import { Category } from './interfaces/category.interface';
@@ -12,6 +15,19 @@ import { Category } from './interfaces/category.interface';
 @Controller('api/v1/categories')
 export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
+
+  @Get()
+  async getCategories(): Promise<Category[]> {
+    return this.categoriesService.getCategories();
+  }
+
+  @Get('/:_id')
+  @UsePipes(ValidationPipe)
+  async getCategoryById(
+    @Param() findParamDTO: FindParamDTO,
+  ): Promise<Category> {
+    return this.categoriesService.getCategoryById(findParamDTO);
+  }
 
   @Post()
   @UsePipes(ValidationPipe)
--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,6 +1,11 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { FindParamDTO } from 'src/shared/dtos/find-param.dto';
 import { CreateCategoryDTO } from './dtos/create-category.dto';
 import { Category } from './interfaces/category.interface';
 
@@ -10,6 +15,22 @@ export class CategoriesService {
     @InjectModel('Category')
     private readonly categoryModel: Model<Category>,
   ) {}
+
+  async getCategories(): Promise<Category[]> {
+    return this.categoryModel.find();
+  }
+
+  async getCategoryById(findParamDTO: FindParamDTO): Promise<Category> {
+    const { _id } = findParamDTO;
+
+    const category = await this.categoryModel.findOne({ _id });
+
+    if (!category) {
+      throw new NotFoundException(`Category with ID "${_id}" not found`);
+    }
+
+    return category;
+  }
 
   async createCategory(
     createCategoryDTO: CreateCategoryDTO,
