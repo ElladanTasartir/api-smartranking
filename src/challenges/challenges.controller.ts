a5bd36f0163c47b853baca5439fd8c1e4f1b5ae9
Erick Malta <erickmalta100@gmail.com>
2020-12-17 01:15:57 -0300

    feat(challenges-controller): add getChallenges method

--- src/challenges/challenges.controller.ts
+++ src/challenges/challenges.controller.ts
@@ -1,12 +1,15 @@
 import {
   Body,
   Controller,
+  Get,
   Post,
+  Query,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { ChallengesService } from './challenges.service';
 import { CreateChallengeDTO } from './dtos/create-challenge.dto';
+import { FindChallengeByPlayerDTO } from './dtos/find-challenge-by-player.dto';
 import { Challenge } from './interfaces/challenge.interface';
 
 @Controller('api/v1/challenges')
@@ -19,5 +22,13 @@ export class ChallengesController {
     @Body() createChallengeDTO: CreateChallengeDTO,
   ): Promise<Challenge> {
     return this.challengesService.createChallenge(createChallengeDTO);
+  }
+
+  @Get()
+  @UsePipes(ValidationPipe)
+  async getChallenges(
+    @Query() findChallengeByPlayerDTO: FindChallengeByPlayerDTO,
+  ): Promise<Challenge[]> {
+    return this.challengesService.getChallenges(findChallengeByPlayerDTO);
   }
 }
