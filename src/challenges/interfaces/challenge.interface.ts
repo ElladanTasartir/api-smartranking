e2d0bd8a06b048d1a17345b88b230f5eb2cef793
Erick Malta <erickmalta100@gmail.com>
2020-12-16 22:25:16 -0300

    feat(interfaces): add Challenge interface and schema

---
+++ src/challenges/interfaces/challenge.interface.ts
@@ -0,0 +1,24 @@
+import { Document } from 'mongoose';
+import { Player } from 'src/players/interfaces/player.interface';
+import { ChallengeStatus } from '../enums/challenge-status.enum';
+
+export interface Challenge extends Document {
+  date: Date;
+  status: ChallengeStatus;
+  requested_at: Date;
+  answered_at: Date;
+  challenger: string;
+  category: string;
+  players: Player[];
+  match: Match;
+}
+
+export interface Match extends Document {
+  def: string;
+  players: Player[];
+  result: Result[];
+}
+
+export interface Result {
+  set: string;
+}
